6f9c100c4d78e4200abcf0bde811daf9dc2d6594
j-jihoon <junji636@gmail.com>
2022-10-10 17:37:30 +0900

    list페이지 작업 중

--- sportCommunity/routes/web.php
+++ sportCommunity/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,4 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('main');
+Route::get('/list', [ListController::class, 'index'])->name('list');
